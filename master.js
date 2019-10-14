70cd35b5cfe5a3f05eb2e847544a183b73fa18b4
eduardorph <eduardorph@hotmail.com>
2019-10-14 16:52:29 -0300

    Inclusão de função getParameterFromUrlByName

--- master.js
+++ master.js
@@ -39,3 +39,14 @@ function esconder_vazio(parent_el){
 	console.log(p);
 	console.log(el_count);
 }
+
+function getParameterFromUrlByName( name ){
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec( window.location.href );
+  if( results == null )
+    return false;
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
